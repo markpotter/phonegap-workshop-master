f4babbdf88c636f413b1a467d154970a5b055c0f
markpotter <mpotter@familysearch.org>
2013-02-11 15:21:18 -0700

    Changed storage to be memory

--- js/main.js
+++ js/main.js
@@ -14,9 +14,9 @@ var app = {
     },
 
     initialize: function() {
-        // this.store = new MemoryStore();
-        this.store = new WebSqlStore();
-        // this.store = new LocalStorageStore();
+        this.store = new MemoryStore();
+        //this.store = new WebSqlStore();
+        //this.store = new LocalStorageStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
