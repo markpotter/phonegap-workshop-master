13fef3433d632063ad521bebabd54edc77e9fdde
markpotter <mpotter@familysearch.org>
2013-02-12 09:51:00 -0700

    Added phonegap.js to application

--- js/main.js
+++ js/main.js
@@ -14,10 +14,21 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+        var self = this;
+        this.store = new MemoryStore(function() {
+            self.showAlert('Store initialize', 'Info');
+        });
         //this.store = new WebSqlStore();
         //this.store = new LocalStorageStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
+    showAlert: function( message, title) {
+        if (navigator.notification){
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ': '+ message): message);
+        }
     }
 
 };
