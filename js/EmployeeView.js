a4f94d1df83aa749062cfde500d2f852de6815d7
markpotter <mpotter@familysearch.org>
2013-03-11 14:04:36 -0600

    added geolocation

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -1,7 +1,23 @@
 var EmployeeView = function(employee){
   this.initialize = function(){
     this.el = $('<div/>');
-    
+
+    //LOCATION API
+    this.el.on('click', '.add-location-btn', this.addLocation);
+  };
+
+  //LOCATION API
+  this.addLocation = function(event) {
+    event.preventDefault();
+    console.log('addLocation');
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+            $('.location', this.el).html(position.coords.latitude + ',' + position.coords.longitude);
+        },
+        function(error) {
+            alert('New - Error getting location '+error.code);
+        });
+    return false;
   };
 
   this.render = function(){
